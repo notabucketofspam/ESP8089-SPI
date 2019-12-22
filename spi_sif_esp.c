683d928871631ac0030a9d613d66dcbfc0022ec5
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2019-12-22 14:15:01 -0500

    Update

--- spi_sif_esp.c
+++ spi_sif_esp.c
@@ -1515,13 +1515,13 @@ int sif_spi_protocol_init(struct spi_device *spi)
   printk("esp8089_spi: fail_count = %d\n", fail_count);
                                 sif_spi_write_raw(spi, tx_buf1, 6);
                                 sif_spi_write_async_read(spi,dummy_tx_buf, rx_buf1,10);
-                                mdelay(100);
+                                //mdelay(100);
   esp_dbg(ESP_DBG_ERROR, "rx:[0x%02x],[0x%02x],[0x%02x],[0x%02x],[0x%02x],[0x%02x],[0x%02x],[0x%02x],[0x%02x],[0x%02x]\n", 
     rx_buf1[0],rx_buf1[1],rx_buf1[2],rx_buf1[3],rx_buf1[4],rx_buf1[5],rx_buf1[6],rx_buf1[7],rx_buf1[8],rx_buf1[9]);
                                 mdelay(100);
 				if(++fail_count > 10)
 					return -ETIMEDOUT;
-                        }while( rx_buf1[2] != 0x01 );
+                        }while( rx_buf1[2] != /*0x01*/ 0x00);
                         //  }while(1);
                  }
                 else if( spi_proto_ini_status == 1 )
